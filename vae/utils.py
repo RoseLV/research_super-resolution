268312371c98f1507d9496efa1522fe172cbe074
Linhan Wang <wanglinhan.pku@gmail.com>
2024-07-05 23:13:49 -0400

    implement vae architecture

---
+++ vae/utils.py
@@ -0,0 +1,27 @@
+from torch import nn
+
+
+def normalization(channels, norm_type: str = "group", num_groups: int = 32):
+    if norm_type == "batch":
+        return nn.BatchNorm3d(channels)
+    elif norm_type == "group":
+        return nn.GroupNorm(num_groups=num_groups, num_channels=channels)
+    elif (not norm_type) or (norm_type.lower() == "none"):
+        return nn.Identity()
+    else:
+        raise NotImplementedError(norm_type)
+
+
+def activation(act_type: str = "swish"):
+    if act_type == "swish":
+        return nn.SiLU()
+    elif act_type == "gelu":
+        return nn.GELU()
+    elif act_type == "relu":
+        return nn.ReLU()
+    elif act_type == "tanh":
+        return nn.Tanh()
+    elif not act_type:
+        return nn.Identity()
+    else:
+        raise NotImplementedError(act_type)
